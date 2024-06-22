d0be75e161cc4d285262ea0e859f1f4a8338de53
jmailia <whatGravity@protonmail.com>
2024-06-22 13:35:52 -0700

    small fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 import ReactMap from 'react-mapbox-gl';
 
+//import ReactMapboxGl, { Layer, Feature } from '../../../';
+
+// tslint:disable-next-line:no-var-requires
+//const data = require('./heatmapData.json');
+// tslint:disable-next-line:no-var-requires
+//const { token, styles } = require('./config.json');
+
+
+
+
 const accessToken = "pk.eyJ1IjoicnlrciIsImEiOiJjbHhjcWxiaDYwZmhrMnFvYWtlbDRlNzFzIn0.u3zAq2Ye9gGAzmkqijKMyQ";
 const style = "mapbox://styles/mapbox/dark-v9";
 
