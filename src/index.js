5b30a042db06fa152d1145b5e7f6abe937bff496
jmailia <whatGravity@protonmail.com>
2024-07-01 20:14:22 -0700

    react update to 18

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
+import App from './App';
 
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+/* const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+ */
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+
+/* 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
-);
+); 
+*/
