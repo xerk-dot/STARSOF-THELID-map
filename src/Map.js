6df8cff2670114783d32bc8d459da4f5fb8172e4
jmailia <whatGravity@protonmail.com>
2024-07-01 23:15:25 -0700

    red heatmap

--- src/Map.js
+++ src/Map.js
@@ -53,19 +53,21 @@ const Map = () => {
             },
             // use sequential color palette to use exponentially as the weight increases
             'heatmap-color': [
-              'interpolate',
-              ['linear'],
-              ['heatmap-density'],
-              0,
-              'rgba(236,222,239,0)',
-              0.2,
-              'rgb(208,209,230)',
-              0.4,
-              'rgb(166,189,219)',
-              0.6,
-              'rgb(103,169,207)',
-              0.8,
-              'rgb(28,144,153)'
+                'interpolate',
+                ['linear'],
+                ['heatmap-density'],
+                0,
+                'rgba(33,102,172,0)',
+                0.2,
+                'rgb(103,169,207)',
+                0.4,
+                'rgb(209,229,240)',
+                0.6,
+                'rgb(253,219,199)',
+                0.8,
+                'rgb(239,138,98)',
+                1,
+                'rgb(178,24,43)'
             ],
             // increase radius as zoom increases
             'heatmap-radius': {
